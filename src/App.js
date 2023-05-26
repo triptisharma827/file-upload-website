3b4acad64457890e975b1590e5a60a3f91ba3780
triptisharma827 <triptisharma827@gmail.com>
2023-05-26 15:20:37 +0530

    files uploaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+
+import vector1 from "./vector1.png";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState("");
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = async () => {
+    if (selectedFile) {
+      try {
+        const fileReader = new FileReader();
+        fileReader.onload = async (e) => {
+          const fileContent = e.target.result;
+          const binaryFileContent = new Uint8Array(fileContent);
+
+          const fileName = selectedFile.name; // Get the file name
+
+          const response = await fetch(
+            `https://i80w84z4s5.execute-api.us-east-1.amazonaws.com/FileUploadProjectLambdaFunction?file_name=${encodeURIComponent(
+              fileName
+            )}`,
+            {
+              method: "POST",
+              body: binaryFileContent,
+            }
+          );
+
+          if (response.ok) {
+            setUploadStatus("File uploaded successfully.");
+          } else {
+            setUploadStatus("Failed to upload file. Invalid size or format");
+          }
+        };
+
+        fileReader.readAsArrayBuffer(selectedFile);
+      } catch (error) {
+        console.error("Error:", error);
+        setUploadStatus("An error occurred while uploading the file.");
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="upload-main">
+      <div className="upload-header">
+
+        <h1 className="main-heading">Store Your files in Cloud</h1>
+        <p className="main-para">
+          Upload your .docx file to store in AWS S3 bucket in .pdf format.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="upload-body">
+      <img src={vector1} className="imag" alt="vector" height={'600px'} />
+      <div className="upload-container">
+        <h2 className="sub-heading">Upload .docx file less than 5mb size </h2>
+        <input type="file" onChange={handleFileChange}  className="file-select"/>
+        <br></br>
+        <button onClick={handleFileUpload} className="buttonClass">Upload</button>
+        <br></br>
+        <div className="upload-status">{uploadStatus}</div>
+      </div>
+      </div>
     </div>
   );
 }
